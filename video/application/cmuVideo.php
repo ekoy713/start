77957b4df9ead9d74fa002e27cd61d7096ebcd27
Eko <ash.lostasia@gmail.com>
2019-01-15 23:19:10 -0500

    initialize video folder

---
+++ video/application/cmuVideo.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<!doctype html>
+<html>
+	<head>
+		<title>Siqing Yang - Video Essay for MSE Application of CMU</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta http-equiv="Pragma" content="no-cache"/>
+        <meta http-equiv="Cache-Control" content="no-cache, no-store" />
+        <meta http-equiv="Expires" content="-1"/>
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	</head>	
+	<body>
+		<div id="login">
+			
+		</div>
+		<div id="content">
+			<div id="videoHolder">
+			</div>
+		</div>
+
+	</body>
+
+	<script type="text/javascript">
+		
+	</script>
+</html>
